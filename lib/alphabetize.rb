f3309abeb3848dab00834ff7cf4c523b82fb3942
Henry Sztanski <henry@sztanski.com>
2019-02-12 19:07:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,9 +6,10 @@ def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
   arr.sort_by do |s|
-    new_arry = s.split("").map do |char|   # ["m", "i", " ", "a"... "n"]
-      esperanto.index(char)
-      #binding.pry
+    #new_arry = s.split("").map do |char|   # ["m", "i", " ", "a"... "n"]
+      #esperanto.index(char)
+
+    esperanto.index(s[0])
     end
     binding.pry
   end
